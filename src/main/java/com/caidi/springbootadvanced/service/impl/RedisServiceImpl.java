e3dfd9f8bb15e530b2f1674032b5641ed8b74c38
caidi <1289359053@qq.com>
2019-05-22 00:04:47 +0800

    v1.1.1.2

--- src/main/java/com/caidi/springbootadvanced/service/impl/RedisServiceImpl.java
+++ src/main/java/com/caidi/springbootadvanced/service/impl/RedisServiceImpl.java
@@ -1,4 +1,26 @@
 package com.caidi.springbootadvanced.service.impl;
 
-public class RedisServiceImpl {
+import com.caidi.springbootadvanced.dao.dbone.MyDao;
+import com.caidi.springbootadvanced.domain.MybaitsUser;
+import com.caidi.springbootadvanced.service.RedisService;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class RedisServiceImpl implements RedisService {
+    @Resource
+    private MyDao myDao;
+
+    @Override
+    public List<MybaitsUser> listMybatisUser() {
+        Example example = new Example(MybaitsUser.class);
+        //排序对象
+        Example.OrderBy orderBy = example.orderBy("uid");
+        orderBy.desc();
+        List<MybaitsUser> mybaitsUsers = myDao.selectByExample(example);
+        return mybaitsUsers;
+    }
 }
