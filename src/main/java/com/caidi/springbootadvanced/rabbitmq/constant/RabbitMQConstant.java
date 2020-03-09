26eb4ffa87eff8f03663d9eb1c798379de4ba80b
caidi <caidif@163.com>
2020-03-09 17:17:43 +0800

    添加rabbitmq变量

---
+++ src/main/java/com/caidi/springbootadvanced/rabbitmq/constant/RabbitMQConstant.java
@@ -0,0 +1,19 @@
+package com.caidi.springbootadvanced.rabbitmq.constant;
+
+/**
+ * @author: 蔡迪
+ * @date: 17:10 2020/3/9
+ * @description: 常变量
+ */
+public class RabbitMQConstant {
+    // 队列1
+    public static final String QUENE_1 = "";
+    // 交换机1
+    public static final String EXCHANGE_1 = "";
+    // key
+    public static final String TOPIC_KEY_1 = "";
+    // key
+    public static final String FANOUT_KEY_1 = "";
+    // key
+    public static final String DIRECT_KEY_1 = "";
+}
